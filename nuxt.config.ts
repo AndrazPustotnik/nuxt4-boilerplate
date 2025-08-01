ba54318ebb2b83ecb43aafb610f510b33f90a404
Andraž Pustotnik <andraz.pustotnik10@gmail.com>
2025-08-01 20:42:52 +0200

    @vueuse/nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon', '@pinia/nuxt', '@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@pinia/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt'
+  ],
   css: ['~/assets/scss/main.scss']
 })
