8fa1b50f705d3a777415cc3f68c59a7a9a498831
Andraž Pustotnik <andraz.pustotnik10@gmail.com>
2025-08-01 21:06:46 +0200

    PlaceholderComponent.vue

---
+++ app/components/PlaceholderComponent.vue
@@ -0,0 +1,10 @@
+<!-- components/PlaceholderComponent.vue -->
+<template>
+  <div class="text-gray-500 italic">
+    Placeholder Component
+  </div>
+</template>
+
+<script setup lang="ts">
+// This is a placeholder component to keep the folder tracked
+</script>
