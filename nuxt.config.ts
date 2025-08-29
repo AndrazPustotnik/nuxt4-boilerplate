534d8ee4f6a2607a749d2b4fa593e6570a1a27c5
Andraž Pustotnik <andraz.pustotnik10@gmail.com>
2025-08-29 20:54:22 +0200

    install tailwind using postcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from "@tailwindcss/vite"
 
 export default defineNuxtConfig({
+  postcss: {
+    plugins: {
+      "@tailwindcss/postcss": {},
+    },
+  },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon", "@pinia/nuxt", "@vueuse/nuxt"],
   css: ["~/assets/scss/main.scss", "~/assets/css/main.css"],
-  vite: {
-    plugins: [tailwindcss()],
-  },
 })
